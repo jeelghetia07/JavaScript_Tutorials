59f0b311a590247d745f807587d99c94dec01595
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-12 23:21:30 +0530

    Create data_types.js

---
+++ 01_Basics/data_types.js
@@ -0,0 +1,31 @@
+"use strict" // treates all JS code in this file as a newer version.
+
+// alert(3+3) // this will show error becoz...its node.js not browser
+
+console.log(3+3)
+
+console.log("Jeel");
+
+let name = "jeel"
+let age = 18
+let isLoggedin = false
+let state;
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol : unique
+
+
+
+object
+*/
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
