cc704abc5ccd72b82968185a63d3baad5b44b477
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-12 23:44:02 +0530

    Create conversion_op.js

---
+++ 01_Basics/conversion_op.js
@@ -0,0 +1,31 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "jeel"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)   // this converts the string to the number.
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+/* 
+"33" => 33
+"33abc" =. NaN
+true => 1; false => 0
+*/
+
+
+// let isLoggedin = 1;
+// let isLoggedin = "";
+let isLoggedin = "Jeel";
+let booleanIsLoggedIn = Boolean(isLoggedin);
+
+console.log(booleanIsLoggedIn);
+
+
