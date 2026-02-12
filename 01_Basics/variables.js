0c3aaf6d86154d25412e4efd82674e9b954c8e6d
jeelghetia07 <jeelghetia07@gmail.com>
2026-02-12 09:18:29 +0530

    Create variables.js

---
+++ 01_Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "jeel@gmail.com"
+var accountPass = "12345"
+accountCity = "Rajkot"
+
+// accountId = 2
+accountEmail = "jeel123@GOOGLE.com"
+accountPass = "22134334"
+accountCity = "Ahmedabad"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountCity])
